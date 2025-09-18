0a5e56f8ba4d48cdf9a13b2a0a4c3f3d5d767021
Manish Jha <jhas67761@gmail.com>
2025-09-18 21:59:24 +0530

    Update Leet_3000_Maxi.Area_DIagonal.java

--- Leet_3000_Maxi.Area_DIagonal.java
+++ Leet_3000_Maxi.Area_DIagonal.java
@@ -5,21 +5,21 @@ class Solution {
     }
     public int areaOfMaxDiagonal(int[][] dimensions) {
         int max = Integer.MIN_VALUE; 
-        Map<Integer,Integer> mp = new HashMap<>(); 
+        ArrayList<int[]> l = new ArrayList<>();
 
         for(int[] arr :dimensions)
         {   
            int res = area(arr); //  Ar.Rect
            int dia = arr[0]*arr[0] + arr[1]*arr[1]; // AR.Diagonal
            max = Math.max(max,dia);  // max select diagonal
-           mp.put(res, dia); 
+          l.add(new int[]{res, dia}); 
         }
         int t = Integer.MIN_VALUE; 
-        for(Map.Entry<Integer,Integer> e : mp.entrySet())
+        for(int[] e : l)
         {
-            if(e.getValue() == max)
+            if(e[1] == max)
             {
-                t = Math.max(e.getKey() , t); 
+                t = Math.max(t,e[0]); 
             }
         }
         return t;
