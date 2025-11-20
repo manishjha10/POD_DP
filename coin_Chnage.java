ea3a1a52b968cc2f0c0e3831a2c8ad004117cedf
Manish Jha <jhas67761@gmail.com>
2025-11-21 00:52:02 +0530

    Update coin_Chnage.java

--- coin_Chnage.java
+++ coin_Chnage.java
@@ -38,3 +38,35 @@ class Solution {
         return (dp[n][amount] == Integer.MAX_VALUE-1 ? -1 : dp[n][amount]);
     }
 }
+
+
+
+//--------------------------------------------------------------------------------------------------------------------------------//
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n  = coins.length; 
+        int[][] dp = new int[n+1][amount+1];  
+        
+        // first row fill 
+        for(int j=0; j<= amount; j++) dp[0][j] = 100000; 
+      
+        // first column  
+        for(int i=0; i<dp.length; i++)  dp[i][0] = 0; 
+
+
+        for(int i=1; i<dp.length; i++)
+        {
+            for(int j=0; j <= amount; j++)
+            {
+                if(coins[i-1] <= j)
+                {
+                    dp[i][j] = Math.min(dp[i][j-coins[i-1]] + 1 , dp[i-1][j]); 
+                }
+                else  dp[i][j] = dp[i-1][j]; 
+            }
+        }
+        return (dp[n][amount] == 100000 ? -1 : dp[n][amount]); 
+    }
+}
+
+// TC: (N * amount) ; 
